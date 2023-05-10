710c8729d7bd8324faa2731b91d4afca3e2bf558
kimpossibleee <yeayoungk@gmail.com>
2023-05-10 03:17:29 -0700

    update registration form and begin sql

--- app/blueprints/auth/routes.py
+++ app/blueprints/auth/routes.py
@@ -1,6 +1,28 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from app.models import User
+from app import db
 from . import bp as app
+from app.forms import RegisterForm, LoginForm
 
 @app.route('/signin')
 def signin():
-    return render_template(signin.jinja)
+    form = LoginForm()
+    return render_template('signin.jinja', form = form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        user = User.query.filter_by(username=username).first()
+        email = User.query.filter_by(email=email)
+        if not email and not user:
+            u = User(username=username, email=email, password=form.password.data)
+            u.commit()
+            flash(f'{username} registered')
+            return redirect(url_for('main.home'))
+        if user:
+            flash(f'{username} already take, please try again')
+        elif email:
+            flash(f'{email} already take, please try again')
+    return render_template('register.jinja', form = form)
